c002bea8a3a830def84ea466aa02b64b1a3d2a9f
Alexander Marval <alexander.marval25@gmail.com>
2024-04-29 10:03:02 -0400

    insert pokemon by batch

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { CreatePokemonDto } from '../pokemon/dto/create-pokemon.dto';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
 
   async executeSeed() {
     const { data } = await axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach(({ name, url }) => {
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log({ name, no });
+      const pokemon: CreatePokemonDto = {
+        name: name,
+        no: no,
+      };
+
+      await this.pokemonModel.create(pokemon);
     });
     return data.results;
   }
