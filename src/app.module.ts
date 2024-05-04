f228fe69e35d198a7d52c45f17e35f0443fed595
Alexander Marval <alexander.marval25@gmail.com>
2024-05-04 11:07:25 -0400

    Add validationSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,13 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
+import { JoiValidationShema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [EnvConfiguration],
+      validationSchema: JoiValidationShema,
     }),
 
     ServeStaticModule.forRoot({
